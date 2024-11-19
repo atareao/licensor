4c377cb4f2a3ad0db73ae3a07ce019a9d663bd4f
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-11-19 06:57:19 +0100

    ✨ Añadidas dependencias y configurado logger y async main en main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod configuration;
+mod template;
+mod variable;
+mod tools;
+
+use simple_logger::init_with_env;
+use log::{info, debug, error};
+use configuration::Configuration;
+
+#[tokio::main]
+async fn main() {
+    info!("Starting...");
+    init_with_env().expect("Cant init logger");
+    let configuration = Configuration::new().await;
+    debug!("{:?}", configuration);
+    let templates: Vec<String> = configuration
+        .templates
+        .iter()
+        .map(|s| s.name.clone())
+        .collect();
+    debug!("{:?}", &templates);
+    let ans: Result<&str, InquireError> = Select::new("Select license?", templates)
+        .prompt();
 }
