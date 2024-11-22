0648805d8ce7f73649dc27df1b154c16b8bacfe7
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-11-22 07:46:58 +0100

    :heavy_plus_sign: Actualizadas y añadidas dependencias en `Cargo.toml`, implementada función de descompresión asíncrona en `src/tools.rs` y mejora en manejo de ficheros.

--- src/tools.rs
+++ src/tools.rs
@@ -18,117 +18,82 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-use log::{error, info, debug};
+use async_zip::base::read::seek::ZipFileReader; //AsyncReadExt
+use log::{debug, error, info};
 use spinners::{Spinner, Spinners};
-use std::{io::Cursor, path::Path, path::PathBuf};
+use std::{io::Cursor, path::Path, path::PathBuf, error::Error};
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-use tokio::fs::{read_dir, File};
-use tokio::io::AsyncWriteExt;
-use async_recursion::async_recursion;
+use tokio::{
+    fs::{create_dir_all, File, OpenOptions},
+    io::BufReader,
+};
+use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
 
 pub async fn fetch_url(url: &str, filename: &str) -> Result<()> {
     info!("fetch_url");
-    let mut spinner = Spinner::new(Spinners::Dots9,
-                                   "Downloading templates".into());
+    let mut spinner = Spinner::new(Spinners::Dots9, "Downloading templates".into());
     let response = reqwest::get(url).await?;
     spinner.stop_and_persist("✔", "Downloaded!".into());
     let mut file = std::fs::File::create(filename)?;
-    let mut content =  Cursor::new(response.bytes().await?);
+    let mut content = Cursor::new(response.bytes().await?);
     std::io::copy(&mut content, &mut file)?;
     Ok(())
 }
 
+pub async fn unzip(zipfile: &str, out_dir: &str) -> Result<()> {
+    info!("unzip");
+    //let file = File::open(zipfile).await?;
+    let out_dir = Path::new(out_dir);
+    let file = File::open(zipfile).await?;
+    let buf_reader = BufReader::new(file).compat();
+    let mut reader = ZipFileReader::new(buf_reader).await?;
+    for index in 0..reader.file().entries().len() {
+        let entry = reader.file().entries().get(index).unwrap();
+        let path = out_dir.join(sanitize_file_path(entry.filename().as_str().unwrap()));
+        // If the filename of the entry ends with '/', it is treated as a directory.
+        // This is implemented by previous versions of this crate and the Python Standard Library.
+        // https://docs.rs/async_zip/0.0.8/src/async_zip/read/mod.rs.html#63-65
+        // https://github.com/python/cpython/blob/820ef62833bd2d84a141adedd9a05998595d6b6d/Lib/zipfile.py#L528
+        let entry_is_dir = entry.dir().unwrap();
 
-#[async_recursion]
-pub async fn get_gitignore(path: &Path) -> Option<PathBuf>{
-    info!("get_gitignore");
-    debug!("Path: {}", &path.to_string_lossy());
-    if exist_gitignore_in_path(path).await || exist_git_in_path(path).await{
-        let mut pathbuf = path.to_path_buf();
-        pathbuf.push(".gitignore");
-        Some(pathbuf)
-    }else if let Some(parent) = path.parent(){
-            get_gitignore(parent).await
-    }else{
-        None
-    }
-}
-pub async fn exist_git_in_path(path: &Path) -> bool{
-    info!("exist_git_in_path");
-    let mut entries = read_dir(path).await.expect("Cant read dir");
-    loop {
-        match entries.next_entry().await{
-            Ok(Some(entry)) => {
-                if entry.file_name() == ".git"{
-                    match entry.file_type().await{
-                        Ok(filetype) => if filetype.is_dir(){
-                            return true;
-                        }
-                        Err(_) => {},
-                    }
+        let mut entry_reader = reader.reader_without_entry(index).await?;
 
-                }
-            },
-            Ok(None) => return false,
-            Err(e) => {
-                error!("error: {}", e);
+        if entry_is_dir {
+            // The directory may have been created if iteration is out of order.
+            if !path.exists() {
+                create_dir_all(&path).await?;
             }
-        }
-    }
-}
-pub async fn exist_gitignore_in_path(path: &Path) -> bool{
-    info!("exist_gitignore_in_path");
-    let mut entries = read_dir(path).await.expect("Cant read dir");
-    loop {
-        match entries.next_entry().await{
-            Ok(Some(entry)) => {
-                if entry.file_name() == ".gitignore"{
-                    match entry.file_type().await{
-                        Ok(filetype) => if filetype.is_file(){
-                            return true;
-                        }
-                        Err(_) => {},
-                    }
-
-                }
-            },
-            Ok(None) => return false,
-            Err(e) => {
-                error!("error: {}", e);
+        } else {
+            // Creates parent directories. They may not exist if iteration is out of order
+            // or the archive does not contain directory entries.
+            let parent = path.parent().ok_or(Box::from("A file entry should have parent directories") as Box<dyn Error + Send + Sync>)?;
+            if !parent.is_dir() {
+                create_dir_all(parent).await?;//.expect("Failed to create parent directories");
             }
+            let writer = OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(&path)
+                .await?;
+                //.expect("Failed to create extracted file");
+            futures_lite::io::copy(&mut entry_reader, &mut writer.compat_write())
+                .await?;
+                //.expect("Failed to copy to extracted file");
+
+            // Closes the file and manipulates its metadata here if you wish to preserve its metadata from the archive.
         }
     }
+
+
+    Ok(())
 }
 
-pub async fn file_exists(path: &str) -> bool{
-    info!("file_exists");
-    if let Ok(metadata) = tokio::fs::metadata(path).await{
-        return metadata.is_dir() || metadata.is_dir() || metadata.is_symlink();
-    }
-    false
+/// Returns a relative path without reserved names, redundant separators, ".", or "..".
+fn sanitize_file_path(path: &str) -> PathBuf {
+    // Replaces backwards slashes
+    path.replace('\\', "/")
+        // Sanitizes each component
+        .split('/')
+        .map(sanitize_filename::sanitize)
+        .collect()
 }
-
-pub async fn add_content_to_file(path: &str, items: Vec<&str>){
-    info!("add_content_to_file");
-    let content: String = if !file_exists(path).await{
-        tokio::fs::read_to_string(path).await.expect("Cant read file")
-    }else{
-        "".to_string()
-    };
-    let mut contents: Vec<&str> = content.split("\n").collect();
-    contents.retain(|item| !item.is_empty());
-    for item in items{
-        if !item.is_empty() && !contents.contains(&item){
-            contents.push(item);
-        }
-    }
-    debug!("open path: {}", path);
-    let mut file = File::create(path).await.expect("Cant read file");
-    let data = contents.join("\n");
-    debug!("content: {}", data);
-    file.write_all(data.as_bytes()).await.expect("Cant write to file");
-    file.write_all(b"\n").await.expect("Cant write to file");
-    file.flush().await.expect("Cant flush file");
-    info!("writed");
-}
-
