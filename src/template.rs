73f27bbe7e0fe91475427b4cc93dab00f7a4995a
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-11-24 08:32:34 +0100

    ✨ Añadido atributo 'description' al struct Template y actualizado su manejo en métodos YAML

--- src/template.rs
+++ src/template.rs
@@ -1,24 +1,37 @@
-use serde::{Serialize, Deserialize};
-use yaml_rust2::{Yaml, yaml::Hash};
+use serde::{Deserialize, Serialize};
+use yaml_rust2::{yaml::Hash, Yaml};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Template{
+pub struct Template {
     pub name: String,
+    pub description: String,
     pub filename: String,
 }
 
 impl Template {
-    pub fn to_yml(&self) -> Yaml{
+    pub fn to_yml(&self) -> Yaml {
         let mut hash = Hash::new();
-        hash.insert(Yaml::String("name".to_string()), Yaml::String(self.name.clone()));
-        hash.insert(Yaml::String("filename".to_string()), Yaml::String(self.filename.clone()));
+        hash.insert(
+            Yaml::String("name".to_string()),
+            Yaml::String(self.name.clone()),
+        );
+        hash.insert(
+            Yaml::String("description".to_string()),
+            Yaml::String(self.description.clone()),
+        );
+        hash.insert(
+            Yaml::String("filename".to_string()),
+            Yaml::String(self.filename.clone()),
+        );
         Yaml::Hash(hash)
     }
-    pub fn from_yml(yml: &Yaml) -> Self{
+    pub fn from_yml(yml: &Yaml) -> Self {
         let name = yml["name"].as_str().unwrap().to_string();
+        let description = yml["description"].as_str().unwrap().to_string();
         let filename = yml["filename"].as_str().unwrap().to_string();
-        Self{
+        Self {
             name,
+            description,
             filename,
         }
     }
@@ -27,20 +40,49 @@ impl Template {
 impl From<Template> for Yaml {
     fn from(template: Template) -> Self {
         let mut hash = Hash::new();
-        hash.insert(Yaml::String("name".to_string()), Yaml::String(template.name.clone()));
-        hash.insert(Yaml::String("filename".to_string()), Yaml::String(template.filename.clone()));
+        hash.insert(
+            Yaml::String("name".to_string()),
+            Yaml::String(template.name.clone()),
+        );
+        hash.insert(
+            Yaml::String("description".to_string()),
+            Yaml::String(template.description.clone()),
+        );
+        hash.insert(
+            Yaml::String("filename".to_string()),
+            Yaml::String(template.filename.clone()),
+        );
         Yaml::Hash(hash)
     }
 }
 impl From<Yaml> for Template {
     fn from(yaml: Yaml) -> Self {
-        yaml.as_hash().map(|hash| {
-            let name = hash.get(&Yaml::String("name".to_string())).unwrap().as_str().unwrap().to_string();
-            let filename = hash.get(&Yaml::String("filename".to_string())).unwrap().as_str().unwrap().to_string();
-            Self{
-            name,
-            filename,
-            }
-        }).unwrap()
+        yaml.as_hash()
+            .map(|hash| {
+                let name = hash
+                    .get(&Yaml::String("name".to_string()))
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string();
+                let description = hash
+                    .get(&Yaml::String("description".to_string()))
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string();
+                let filename = hash
+                    .get(&Yaml::String("filename".to_string()))
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string();
+                Self {
+                    name,
+                    description,
+                    filename,
+                }
+            })
+            .unwrap()
     }
 }
